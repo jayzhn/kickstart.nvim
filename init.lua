4122d539921331b7c310cbc7f14c977cdeca4289
JJ <jiayizhan2005@gmail.com>
2025-01-21 22:49:58 +0100

    -

--- init.lua
+++ init.lua
@@ -546,6 +546,7 @@ require('lazy').setup({
         emmet_ls = {},
         eslint = {},
         cssls = {},
+        angularls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -889,6 +890,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.auto-save',
+  require 'kickstart.plugins.colorizer',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
